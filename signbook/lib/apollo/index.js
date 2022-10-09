e238dad1d71ed4ebc65f0deed4adeb915c7351c6
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-10-10 00:31:27 +0300

    Configuration of Apollo Client

--- signbook/lib/apollo/index.js
+++ signbook/lib/apollo/index.js
@@ -4,10 +4,38 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 let url = 'https://rwjssignbook/herohuapp.com/v1/graphql';
 let apolloClient;
 
+// Creating an Apollo Client
 function createApolloClient() {
     return new ApolloClient({
         ssrMode: typeof window === "undefined",
         link: new HttpLink({ url }),
         cache: new InMemoryCache(),
     });
+}
+
+// Initialize Apollo Client
+// Avoiding recreating a new Apollo client for each page.
+export function initApollo(initialState = null) {
+    const client = apolloClient || createApolloClient();
+
+    if (initialState) {
+        client.cache.restore({
+            ...client.extract(),
+            ...initialState
+        });
+    }
+
+    if (typeof window === "undefined") {
+        return client;
+    }
+
+    if (!apolloClient) {
+        apolloClient = client;
+    }
+
+    return client;
+}
+
+export function useApollo(initialState) {
+    return useMemo(() => initApollo(initialState), [initialState]);
 }
--- signbook/pages/_app.js
+++ signbook/pages/_app.js
@@ -1,7 +1,20 @@
+import Head from 'next/head';
+import { ApolloProvider } from '@apollo/client';
+import { useApollo } from '../lib/apollo';  
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// Wrapping the whole app using the Apollo context provider
+export function App({ Component, pageProps }) {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Head>
+        <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
 }
 
-export default MyApp
+export default App;
