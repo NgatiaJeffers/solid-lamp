93e132ba31c5dc6243b817dae2fcc715096ea1e5
Jefferson Gakuya <jeffersongakuya@gmail.com>
2022-10-24 22:23:33 +0300

    Set up redux store

--- reduxshop/pages/_app.js
+++ reduxshop/pages/_app.js
@@ -1,7 +1,24 @@
+import { Head } from "next/head";
+import { Provider } from "react-redux";
+import { useStore } from "../redux/store";
+
+import Navbar from "../components/Navbar";
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const store = useStore(pageProps.initialReduxState)
+
+  return (
+    <>
+      <Provider store={store}>
+        <Navbar />
+        <div className="w-9/12 m-auto pt-10">
+          <Component {...pageProps} />
+        </div>
+      </Provider>
+    </>
+  )
+
 }
 
 export default MyApp
---
+++ reduxshop/redux/store.js
@@ -0,0 +1,63 @@
+import { useMemo } from "react";
+import { applyMiddleware, configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "@reduxjs/toolkit/dist/devtoolsExtension";
+
+// variable for keeping the redux store
+let store;
+
+// Initializing our redux store
+const initialState = {};
+
+// Creating out reducer
+const reducer = (state = initialState, action) => {
+    const itemID = action.id;
+
+    switch (action.type) {
+        case "INCREMENT":
+            const newItemAmount = itemID in state ? state[itemID] + 1 : 1;
+            return { ...state, [itemID]: newItemAmount, };
+        case "DECREMENT":
+            if (state?.[itemID] > 0) {
+                return { ...state, [itemID]: state[itemID] - 1, };
+            }
+            return state;
+        default:
+            return state;
+    }
+}
+
+// Fully initializing our store
+function initStore(preloadedState = initialState) {
+    return configureStore(
+        reducer,
+        preloadedState,
+        composeWithDevTools(applyMiddleware())
+    )
+}
+
+// Second function to properly intialize the store
+export const initializeStore = (preloadedState) => {
+    let _store = store ?? initStore(preloadedState);
+
+    if (preloadedState && store) {
+        _store = initStore({
+            ...store.getState(),
+            ...preloadedState,
+        });
+        store = undefined;
+    }
+
+    // Return "_store" when intializing Redux on the server-side
+    if (typeof window === "undefined") return store;
+    if (!store) store = _store;
+
+    return _store;
+}
+
+// A hook wrapped using "useMemo" which will cache complex initial state,
+// avoiding the system re-parsing it every "useStore" function call.
+export function useStore(initialState) {
+    return useMemo(
+        () => initializeStore(initialState), [initialState]
+    );
+}
